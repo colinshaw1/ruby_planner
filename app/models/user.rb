e5c1704d5b435edf6546e3306d9b89b6459c5c85
Colin Shaw <colin.shaw400@outlook.com>
2023-01-14 09:39:45 +0000

    add has_many relationships to users for associations to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+          # add many relationships for todo list and contacts for each user
+          has_many :contacts
+          has_many :todos
 end
