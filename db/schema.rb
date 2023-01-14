72f864299c03ed5134174decb1027120f1a7aa18
Colin Shaw <colin.shaw400@outlook.com>
2023-01-14 09:51:04 +0000

    add user id to contacts and todo list databases and migrating changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_14_094533) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_094848) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -22,21 +22,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_14_094533) do
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
-  create_table "conts", force: :cascade do |t|
-    t.string "frist_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "todos", force: :cascade do |t|
     t.string "task"
     t.string "description"
     t.string "timeframe"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
