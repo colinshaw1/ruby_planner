89f9443e8d795028042d7cb4a8ee733b3c4c4a16
Colin Shaw <colin.shaw400@outlook.com>
2023-01-14 08:00:27 +0000

    add routes for sign in, sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # new routes for devise users from gem
   devise_for :users
-  resources :conts
+  # resources for routes in to do list
   resources :todos
+  # resources for routes in contacts
   resources :contacts
-  get 'home/about'
   # get 'home/index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
